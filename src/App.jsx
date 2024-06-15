4f2856e13e9777ea19904e110399d0501233aeb4
patelji97 <166973311+patelji97@users.noreply.github.com>
2024-06-15 16:39:24 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+ name: "Disha Patel",
+  title: "Web Developer & React js Developer",
+  email: "divya0304patel@gmail.com",
+  gitHub: "patelji97",
+  instagram: "",
+  linkedIn: "disha-patel ",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
